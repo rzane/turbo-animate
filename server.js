9bc14fd0b015cbef3b5d94963799eea744a8c56d
Ray Zane <raymondzane@gmail.com>
2023-11-16 11:49:08 -0500

    Add redirect and form submission examples

--- server.js
+++ server.js
@@ -4,19 +4,17 @@ import nunjucks from "nunjucks";
 const app = express();
 const port = parseInt(process.env.PORT || "9000")
 
-app.get("/", (_req, res) => {
-  res.render("index.html");
-});
-
-app.get("/about", (_req, res) => {
-  res.render("about.html");
-});
+app.get("/", (_req, res) => res.render("index.html"));
+app.get("/about", (_req, res) => res.render("about.html"));
+app.get("/redirect", (_req, res) => res.redirect("/about"));
+app.get("/form", (_req, res) => res.render("form.html"));
+app.post("/form", (_req, res) => res.redirect(303, "/"));
 
 app.get("/turbo-animate.js", (_req, res) => {
   res.sendFile("turbo-animate.js", { root: process.cwd() });
 });
 
-nunjucks.configure('example', {
+nunjucks.configure("example", {
   autoescape: true,
   express: app
 });
