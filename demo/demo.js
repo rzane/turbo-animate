fe7d6cb68bcd03105ad5a52d5576529656872a79
Ray Zane <raymondzane@gmail.com>
2023-11-16 10:53:15 -0500

    First commit

---
+++ demo/demo.js
@@ -0,0 +1,4 @@
+import "https://cdn.skypack.dev/@hotwired/turbo@^7.3.0";
+import { animateTurbo } from "/index.js";
+
+animateTurbo();
---
+++ index.js
@@ -0,0 +1,34 @@
+async function animate(className) {
+  const elements = Array.from(document.querySelectorAll("[data-turbo-animate]"));
+
+  const promises = elements.map(async element => {
+    element.classList.add(className);
+    await Promise.all(element.getAnimations().map(animation => animation.finished));
+    element.classList.remove(className);
+  });
+
+  await Promise.all(promises);
+}
+
+function onVisit(visitEvent) {
+  const action = visitEvent.detail.action;
+  const leave = animate(`turbo-${action}-leave`);
+
+  const onBeforeRender = async (event) => {
+    event.preventDefault();
+    await leave;
+    event.detail.resume();
+  };
+
+  const onLoad = async () => {
+    await animate(`turbo-${action}-enter`);
+    animateTurbo();
+  };
+
+  addEventListener("turbo:before-render", onBeforeRender, { once: true });
+  addEventListener("turbo:load", onLoad, { once: true });
+}
+
+export function animateTurbo() {
+  addEventListener("turbo:visit", onVisit, { once: true });
+}
