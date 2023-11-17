14d412caffdf57e3c7a00b5e42da8f0787de5faa
Ray Zane <raymondzane@gmail.com>
2023-11-16 20:08:41 -0500

    Add slow response examples

--- example/server.js
+++ example/server.js
@@ -3,12 +3,23 @@ import nunjucks from "nunjucks";
 
 const app = express();
 const port = parseInt(process.env.PORT || "9000")
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 app.get("/", (_req, res) => res.render("index.html"));
 app.get("/about", (_req, res) => res.render("about.html"));
 app.get("/redirect", (_req, res) => res.redirect("/about"));
 app.get("/form", (_req, res) => res.render("form.html"));
 app.post("/form", (_req, res) => res.redirect(303, "/"));
+
+app.get("/slow", async (_req, res) => {
+  await sleep(1000);
+  res.render("about.html");
+});
+
+app.get("/very-slow", async (_req, res) => {
+  await sleep(4000);
+  res.render("about.html");
+});
 
 app.get("/turbo-animate.js", (_req, res) => {
   res.sendFile("turbo-animate.js", { root: process.cwd() });
